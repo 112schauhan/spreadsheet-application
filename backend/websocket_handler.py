634b22098b17235371368b1ecea71ad176e8a4a9
112schauhan <surbhi.surbhisingh.singh13@gmail.com>
2025-07-29 01:39:33 +0530

    feat:Addition of web socket handler

---
+++ backend/websocket_handler.py
@@ -0,0 +1,76 @@
+from fastapi import WebSocket, WebSocketDisconnect
+from typing import Dict, List
+import json
+from collaboration import CollaborationService
+from user import UserService
+from spreadsheet import SpreadsheetService
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: Dict[str, List[WebSocket]] = {}
+        self.user_service = UserService()
+        self.spreadsheet_service = SpreadsheetService()
+        self.collaboration_service = CollaborationService(
+            self.spreadsheet_service)
+        self.usernames: Dict[str, Dict[WebSocket, str]] = {}
+
+    async def connect(self, websocket: WebSocket, sheet_id: str):
+        await websocket.accept()
+        if sheet_id not in self.active_connections:
+            self.active_connections[sheet_id] = []
+            self.usernames[sheet_id] = {}
+        self.active_connections[sheet_id].append(websocket)
+
+    def disconnect(self, websocket: WebSocket, sheet_id: str):
+        self.active_connections[sheet_id].remove(websocket)
+        self.usernames[sheet_id].pop(websocket, None)
+
+    async def broadcast(self, sheet_id: str, message: dict):
+        if sheet_id in self.active_connections:
+            data = json.dumps(message)
+            for connection in self.active_connections[sheet_id]:
+                await connection.send_text(data)
+
+    async def receive(self, websocket: WebSocket, sheet_id: str):
+        try:
+            while True:
+                data = await websocket.receive_text()
+                message = json.loads(data)
+                await self.handle_message(sheet_id, websocket, message)
+        except WebSocketDisconnect:
+            self.disconnect(websocket, sheet_id)
+            # Optionally broadcast user left
+
+    async def handle_message(self, sheet_id: str, websocket: WebSocket, message: dict):
+        # Example message types: 'join', 'cell_update', 'cursor_update', 'user_presence'
+        msg_type = message.get("type")
+        if msg_type == "join":
+            username = message.get("username", "Anonymous")
+            user = self.user_service.add_user(sheet_id, username)
+            self.usernames[sheet_id][websocket] = user.user_id
+            await self.broadcast(sheet_id, {"type": "user_presence", "users": [u.username for u in self.user_service.get_users(sheet_id)]})
+        elif msg_type == "cell_update":
+            cell_ref = message.get("cellRef")
+            value = message.get("value")
+            formula = message.get("formula")
+            user_id = self.usernames[sheet_id].get(websocket)
+            cell = self.collaboration_service.apply_edit(
+                sheet_id, cell_ref, value, formula, user_id)
+            # Broadcast updated cell to all clients
+            await self.broadcast(sheet_id, {
+                "type": "cell_update",
+                "cellRef": cell_ref,
+                "value": cell.value,
+                "formula": cell.formula,
+                "version": cell.version,
+                "userId": user_id,
+            })
+        elif msg_type == "cursor_update":
+            user_id = self.usernames[sheet_id].get(websocket)
+            cursor_pos = message.get("position")
+            await self.broadcast(sheet_id, {
+                "type": "cursor_update",
+                "userId": user_id,
+                "position": cursor_pos,
+            })
