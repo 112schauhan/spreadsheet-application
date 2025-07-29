7ab44ae03872921d69a5f6919f2659741eb9a5a8
112schauhan <surbhi.surbhisingh.singh13@gmail.com>
2025-07-30 03:49:06 +0530

    feat: Improve WebSocket handler for real-time collaboration

--- backend/websocket_handler.py
+++ backend/websocket_handler.py
@@ -23,8 +23,13 @@ class ConnectionManager:
         self.active_connections[sheet_id].append(websocket)
 
     def disconnect(self, websocket: WebSocket, sheet_id: str):
-        self.active_connections[sheet_id].remove(websocket)
-        self.usernames[sheet_id].pop(websocket, None)
+        if sheet_id in self.active_connections:
+            self.active_connections[sheet_id].remove(websocket)
+            
+            # Remove user from user service if they had a username
+            if sheet_id in self.usernames and websocket in self.usernames[sheet_id]:
+                user_id = self.usernames[sheet_id].pop(websocket)
+                self.user_service.remove_user(sheet_id, user_id)
 
     async def broadcast(self, sheet_id: str, message: dict):
         if sheet_id in self.active_connections:
@@ -40,16 +45,83 @@ class ConnectionManager:
                 await self.handle_message(sheet_id, websocket, message)
         except WebSocketDisconnect:
             self.disconnect(websocket, sheet_id)
-            # Optionally broadcast user left
+            # Broadcast updated user list after disconnect
+            users = self.user_service.get_users(sheet_id)
+            await self.broadcast(sheet_id, {
+                "type": "user_presence", 
+                "users": [
+                    {
+                        "userId": u.user_id,
+                        "username": u.username,
+                        "color": u.color,
+                        "status": "online",
+                        "lastActive": 0
+                    } for u in users
+                ]
+            })
 
     async def handle_message(self, sheet_id: str, websocket: WebSocket, message: dict):
-        # Example message types: 'join', 'cell_update', 'cursor_update', 'user_presence', 'comment_add', 'history_request'
+        # Example message types: 'join', 'user_join', 'user_leave', 'cell_update', 'cursor_update', 'user_presence', 'comment_add', 'history_request'
         msg_type = message.get("type")
         if msg_type == "join":
             username = message.get("username", "Anonymous")
             user = self.user_service.add_user(sheet_id, username)
             self.usernames[sheet_id][websocket] = user.user_id
             await self.broadcast(sheet_id, {"type": "user_presence", "users": [u.username for u in self.user_service.get_users(sheet_id)]})
+        elif msg_type == "user_join":
+            # Handle authenticated user joining
+            user_data = message.get("user", {})
+            username = user_data.get("username", "Anonymous")
+            user_id = user_data.get("userId", username)
+            color = user_data.get("color", "#3b82f6")
+            
+            # Add user to collaboration service with custom ID and color
+            user = self.user_service.add_user(sheet_id, username, user_id, color)
+            self.usernames[sheet_id][websocket] = user_id
+            
+            # Broadcast updated user list to all clients
+            users = self.user_service.get_users(sheet_id)
+            await self.broadcast(sheet_id, {
+                "type": "user_presence", 
+                "users": [
+                    {
+                        "userId": u.user_id,
+                        "username": u.username,
+                        "color": u.color,
+                        "status": "online",
+                        "lastActive": user_data.get("lastActive", 0)
+                    } for u in users
+                ]
+            })
+        elif msg_type == "user_leave":
+            # Handle authenticated user leaving
+            user_id = message.get("userId")
+            if user_id and sheet_id in self.usernames:
+                # Find and remove the user's websocket connection
+                websocket_to_remove = None
+                for ws, uid in self.usernames[sheet_id].items():
+                    if uid == user_id:
+                        websocket_to_remove = ws
+                        break
+                
+                if websocket_to_remove:
+                    self.usernames[sheet_id].pop(websocket_to_remove, None)
+                    self.user_service.remove_user(sheet_id, user_id)
+                    
+                    # Broadcast updated user list
+                    users = self.user_service.get_users(sheet_id)
+                    await self.broadcast(sheet_id, {
+                        "type": "user_presence", 
+                        "users": [
+                            {
+                                "userId": u.user_id,
+                                "username": u.username,
+                                "color": u.color,
+                                "status": "online",
+                                "lastActive": 0
+                            } for u in users
+                        ]
+                    })
         elif msg_type == "cell_update":
             cell_ref = message.get("cellRef")
             value = message.get("value")
